6d283cae74133efa9507a5f03dd3469a800a1c06
Dylan.dan <danxin923@pingan.com.cn>
2017-02-06 16:57:28 +0800

    file operation

---
+++ src/com/dylan/scala/file/OperationFile.scala
@@ -0,0 +1,50 @@
+package com.dylan.scala.file
+import scala.io.Source
+import java.io.PrintWriter
+import sys.process._
+
+object OperationFile extends App {
+  
+//  val source = Source.fromFile("C:\\Users\\dylan.dan\\Desktop\\dy.txt","UTF-8");
+  
+//  val lineiterator = source.getLines;
+  
+//  for(c <- source) println(c)
+  
+//  for(l <- lineiterator) println(l)
+  
+//  var value = readInt();
+  
+//  println(value)
+  
+//  var n = 0
+//  
+//  while(n < 10){
+//    
+//    var value = readLine();
+//  
+//     println(value)
+//    
+//     n += 1
+//  }
+  
+//  var baidusource = Source.fromURL("https://www.baidu.com","UTF-8")
+//	var baidusource = Source.fromURL("http://www.linuxidc.com/Linux/2015-08/121033.htm","UTF-8")
+//  
+//  val baidulineiterator = baidusource.getLines;
+//  
+//  for(l <- baidulineiterator) println(l)
+  
+//   val out = new PrintWriter("C:\\Users\\dylan.dan\\Desktop\\dx.txt");
+//   for(i <- 1 to 100) out.println(i);
+//   out.close()
+  
+     val res = "ls "!!
+     
+     print(res)
+   
+  
+  
+  
+  
+}
